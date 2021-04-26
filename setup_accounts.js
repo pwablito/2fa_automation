1b7d915f1fb1489be84186f6cca593c613e31b4a
pwablito <pwablito@gmail.com>
2021-04-26 16:24:41 -0600

    Add frontend structure for git process

--- setup_accounts.js
+++ setup_accounts.js
@@ -1,13 +1,14 @@
 $("#setup_accounts_button").click(() => {
-    $("#select_accounts_div").hide();
-    $("#setup_accounts_div").show();
-
-    let boxes = $(".checkbox");
-    boxes.each((index) => {
-        if (boxes[index].checked) {
-            initiate_setup($(boxes[index]).data("service"));
-        }
-    });
+    if (!$("#setup_accounts_button").hasClass("disabled")) {
+        $("#select_accounts_div").hide();
+        $("#setup_accounts_div").show();
+        let boxes = $(".checkbox");
+        boxes.each((index) => {
+            if (boxes[index].checked) {
+                initiate_setup($(boxes[index]).data("service"));
+            }
+        });
+    }
 });
 
 // Dispatcher
@@ -33,7 +34,7 @@ function initiate_twitter_setup() {
     console.log("Setting up twitter");
     $("#setup_processes_list").append(
         `
-        <li class="gray">This is the Twitter setup section</li>
+        <div class="gray">This is the Twitter setup section</div>
         `
     );
 }
@@ -42,7 +43,7 @@ function initiate_reddit_setup() {
     console.log("Setting up reddit");
     $("#setup_processes_list").append(
         `
-        <li class="gray">This is the Reddit setup section</li>
+        <div class="gray">This is the Reddit setup section</div>
         `
     );
 }
@@ -51,16 +52,55 @@ function initiate_github_setup() {
     console.log("Setting up github");
     $("#setup_processes_list").append(
         `
-        <li class="gray">This is the Github setup section</li>
+        <div class="gray">
+            <div class="row">
+                <div class="col-3">Github</div>
+                <div class="col-9">
+                    <div id="github_setup_div" class="row"></div>
+                </div>
+            </div>
+        </div>
         `
     );
+    $("#github_setup_div").append(
+        `
+        <input type=text id="github_phone_number_input" placeholder="Phone number">
+        <button class="btn btn-success" id="github_phone_number_button">Submit</button>
+        `
+    );
+    $("#github_phone_number_button").click(function() {
+        let number = $("#github_phone_number_input").val();
+        if (number) {
+            // Send to backend, proceed
+            $("#github_setup_div").empty();
+            $("#github_setup_div").append(
+                `
+                <input type=text id="github_code_input" placeholder="Code from SMS">
+                <button class="btn btn-success" id="github_code_button">Submit</button>
+                `
+            );
+            $("#github_code_button").click(function() {
+
+                let code = $("#github_code_input").val();
+                if (code) {
+                    // send to backend, proceed
+                    $("#github_setup_div").empty();
+                    $("#github_setup_div").append(
+                        `
+                        Finished!
+                        `
+                    );
+                }
+            });
+        }
+    });
 }
 
 function initiate_google_setup() {
     console.log("Setting up google");
     $("#setup_processes_list").append(
         `
-        <li class="gray">This is the Google setup section</li>
+        <div class="gray">This is the Google setup section</div>
         `
     );
 }
@@ -69,7 +109,7 @@ function initiate_pinterest_setup() {
     console.log("Setting up pinterest");
     $("#setup_processes_list").append(
         `
-        <li>This is the Pinterest setup section</li>
+        <div class="gray">This is the Pinterest setup section</div>
         `
     );
 }
