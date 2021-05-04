af2e802559653948e6f8175ad97ae51a27679bae
pwablito <pwablito@gmail.com>
2021-05-03 18:34:19 -0600

    Create Google 2FA driver

---
+++ google.js
@@ -0,0 +1,106 @@
+console.log("google.js injected");
+
+function getElementByXpath(doc, xpath) {
+    return doc.evaluate(xpath, doc, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
+function change(field, value) {
+    field.value = value;
+    field.dispatchEvent(new Event('input', { bubbles: true }));
+    field.dispatchEvent(new Event('change', { bubbles: true }));
+    field.dispatchEvent(new KeyboardEvent('keydown', { bubbles: true, cancelable: false, key: '', char: '' }));
+    field.dispatchEvent(new KeyboardEvent('keypress', { bubbles: true, cancelable: false, key: '', char: '' }));
+    field.dispatchEvent(new KeyboardEvent('keyup', { bubbles: true, cancelable: false, key: '', char: '' }));
+}
+
+chrome.runtime.onMessage.addListener(
+    function(request, _) {
+        if (request.google_username) {
+            document.querySelector("#identifierId").value = request.username;
+            document.querySelector("#identifierNext > div > button").click();
+            chrome.runtime.sendMessage({
+                "google_get_password": true
+            });
+        }
+        if (request.google_phone_number) {
+            document.querySelector("c-wiz > div > div:nth-child(3) > c-wiz > div > div > div:nth-child(3) > div > div > div > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div > div:nth-child(2) > div > div > div > input").value = request.number;
+            document.querySelector("c-wiz > div > div:nth-child(3) > c-wiz > div > div > div:nth-child(3) > div:nth-child(2) > div > div:nth-child(3) > div").click();
+
+            setTimeout(() => {
+                if (document.querySelector("c-wiz > div > div:nth-child(3) > c-wiz > div > div > div:nth-child(3) > div > div > div > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div > div:nth-child(2) > div > div > div > input") == null) {
+                    chrome.runtime.sendMessage({
+                        "google_get_code": true
+                    })
+                } else {
+                    chrome.runtime.sendMessage({
+                        google_get_phone_number: true,
+                        message: "Try again with a different phone number"
+                    })
+                }
+            }, 3000);
+        }
+        if (request.google_code) {
+            document.querySelector("c-wiz > div > div:nth-child(3) > c-wiz > div > div > div:nth-child(3) > div > div > div > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div > div > div > div > input").value = request.code;
+            document.querySelector("c-wiz > div > div:nth-child(3) > c-wiz > div > div > div:nth-child(3) > div:nth-child(2) > div > div:nth-child(3) > div").click()
+            setTimeout(() => {
+                if (document.querySelector("c-wiz > div > div:nth-child(3) > c-wiz > div > div > div:nth-child(3) > div > div > div > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div > div > div > div > input") != null) {
+                    chrome.runtime.sendMessage({
+                        google_get_code: true,
+                        message: "Incorrect code"
+                    })
+                } else {
+                    document.querySelector("c-wiz > div > div:nth-child(3) > c-wiz > div > div > div:nth-child(3) > div:nth-child(2) > div > div:nth-child(3) > div:nth-child(2)").click();
+                }
+            }, 4000);
+        }
+        if (request.google_password) {
+            document.querySelector("#password > div > div > div > input").value = request.password;
+            document.querySelector("#passwordNext > div > button").click();
+            setTimeout(() => {
+                if (document.querySelector("#password > div > div > div > input") != null) {
+                    chrome.runtime.sendMessage({
+                        google_get_password: true,
+                        message: "Incorrect password"
+                    })
+                }
+            }, 5000);
+        }
+    }
+);
+
+if (window.location.href.includes("myaccount.google.com/signinoptions/two-step-verification/enroll-welcome")) {
+    if (document.querySelector("c-wiz > div > div:nth-child(3) > c-wiz > div > div > div > div:nth-child(3) > div > div:nth-child(2) > div > div") != null) {
+        document.querySelector("c-wiz > div > div:nth-child(3) > c-wiz > div > div > div > div:nth-child(3) > div > div:nth-child(2) > div > div").click();
+    } else {
+        document.querySelector("c-wiz > div > div:nth-child(3) > c-wiz > div > div > div:nth-child(3) > div:nth-child(2) > div > div").click()
+    }
+} else if (window.location.href.includes("accounts.google.com/signin/v2/challenge/pwd")) {
+    chrome.runtime.sendMessage({
+        "google_get_password": true
+    });
+} else if (window.location.href.includes("myaccount.google.com/signinoptions/two-step-verification/enroll")) {
+    if (document.querySelector("c-wiz > div > div:nth-child(3) > c-wiz > div > div > div:nth-child(3) > div > div > div > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div > div:nth-child(2) > div > div > div > input") != null) {
+        chrome.runtime.sendMessage({
+            "google_get_phone_number": true
+        });
+    }
+} else if (window.location.href.includes("myaccount.google.com/signinoptions/two-step-verification?")) {
+    chrome.runtime.sendMessage({
+        "google_finished": true
+    });
+} else if (window.location.href.includes("myaccount.google.com/intro/security")) {
+    document.querySelector("c-wiz > div > div:nth-child(2) > c-wiz > c-wiz > div > div:nth-child(3) > div > div > c-wiz > section > div > div > div > div > div > div > div > div:nth-child(4) > div > a").click();
+} else if (window.location.href.includes("/identifier")) {
+    chrome.runtime.sendMessage({
+        "google_username": true
+    });
+} else if (window.location.href.includes("myaccount.google.com/security")) {
+    window.location.href = "https://myaccount.google.com/signinoptions/two-step-verification/enroll-welcome";
+} else if (window.location.href.includes("accounts.google.com/ServiceLogin/signinchooser")) {
+    document.querySelector("#view_container > div > div > div:nth-child(2) > div > div > div > form > span > section > div > div > div > div > ul > li:nth-child(2) > div").click();
+    setTimeout(() => {
+        chrome.runtime.sendMessage({
+            "google_get_username": true
+        });
+    }, 5000);
+}
--- setup_accounts.js
+++ setup_accounts.js
@@ -49,7 +49,6 @@ function initiate_twitter_setup() {
 
     chrome.runtime.onMessage.addListener(
         function(request, sender) {
-            console.log(request, sender);
             if (request.twitter_logged_in != null) {
                 if (request.twitter_logged_in) {
                     $("#twitter_setup_div").html(
@@ -98,10 +97,10 @@ function initiate_twitter_setup() {
             } else if (request.twitter_get_password) {
                 $("#twitter_setup_div").html(
                     `
-                        <p>Please enter your password</p>
-                        <input type=password id="twitter_password_input">
-                        <button class="btn btn-success" id="twitter_password_button">Submit</button>
-                        `
+                    <p>Please enter your password</p>
+                    <input type=password id="twitter_password_input">
+                    <button class="btn btn-success" id="twitter_password_button">Submit</button>
+                    `
                 );
                 $("#twitter_password_button").click(function() {
                     let password = $("#twitter_password_input").val();
@@ -184,7 +183,6 @@ function initiate_github_setup() {
 
     chrome.runtime.onMessage.addListener(
         function(request, sender) {
-            console.log(request, sender);
             if (request.github_logged_in !== null) {
                 if (request.github_logged_in) {
                     $("#github_setup_div").html(
@@ -295,6 +293,113 @@ function initiate_google_setup() {
             </div>
         </div>
         `
+    );
+    $("#google_setup_div").html(`Please wait...`);
+    chrome.windows.create({
+        url: "https://myaccount.google.com/signinoptions/two-step-verification/enroll-welcome",
+        focused: false,
+        // state: "minimized"
+    });
+
+
+    chrome.runtime.onMessage.addListener(
+        function(request, sender) {
+            if (request.google_error) {
+                $("#google_setup_div").html(
+                    `
+                    <p>${request.message}</p>
+                    `
+                );
+            } else if (request.google_wrong_password) {
+
+            } else if (request.google_get_password) {
+                $("#google_setup_div").html(
+                    `
+                    ${request.message != null ? "<p>" + request.message + "</p>" : ""}
+                    <p>Please enter your password</p>
+                    <input type=password id="google_password_input">
+                    <button class="btn btn-success" id="google_password_button">Submit</button>
+                    `
+                );
+                $("#google_password_button").click(function() {
+                    let password = $("#google_password_input").val();
+                    if (password) {
+                        chrome.tabs.sendMessage(
+                            sender.tab.id, {
+                                google_password: true,
+                                password: password
+                            }
+                        );
+                    }
+                    $("#google_setup_div").html(`Please wait...`);
+                });
+            } else if (request.google_get_phone_number) {
+                $("#google_setup_div").html(
+                    `
+                    ${request.message != null ? "<p>" + request.message + "</p>" : ""}
+                    <p>Please enter your phone number</p>
+                    <input type=text id="google_phone_number_input" placeholder="Phone number">
+                    <button class="btn btn-success" id="google_phone_number_button">Submit</button>
+                    `
+                );
+                $("#google_phone_number_button").click(function() {
+                    let number = $("#google_phone_number_input").val();
+                    if (number) {
+                        chrome.tabs.sendMessage(
+                            sender.tab.id, {
+                                google_phone_number: true,
+                                number: number
+                            }
+                        );
+                        $("#google_setup_div").html(`Please wait...`);
+                    }
+                });
+            } else if (request.google_get_code) {
+                $("#google_setup_div").html(
+                    `
+                    ${request.message != null ? "<p>" + request.message + "</p>" : ""}
+                    <p>Please enter the code sent to your phone</p>
+                    G-<input type=text id="google_code_input" placeholder="Code">
+                    <button class="btn btn-success" id="google_code_button">Submit</button>
+                    `
+                );
+                $("#google_code_button").click(function() {
+                    let code = $("#google_code_input").val();
+                    if (code) {
+                        chrome.tabs.sendMessage(
+                            sender.tab.id, {
+                                google_code: true,
+                                code: code
+                            }
+                        );
+                    }
+                    $("#google_setup_div").html(`Please wait...`);
+                });
+            } else if (request.google_get_username) {
+                $("#google_setup_div").html(
+                    `
+                    <p>Please enter your username</p>
+                    <input type=text id="google_username_input" placeholder="Username">
+                    <button class="btn btn-success" id="google_username_button">Submit</button>
+                    `
+                );
+                $("#google_username_button").click(function() {
+                    let username = $("#google_username_input").val();
+                    if (username) {
+                        chrome.tabs.sendMessage(
+                            sender.tab.id, {
+                                google_username: true,
+                                username: username
+                            }
+                        );
+                        $("#google_setup_div").html(`Please wait...`);
+                    }
+                });
+            } else if (request.google_finished) {
+                chrome.tabs.remove(sender.tab.id);
+                $("#google_setup_div").html(`Finished setting up Google`);
+            }
+        }
     );
 }
 // END GOOGLE
