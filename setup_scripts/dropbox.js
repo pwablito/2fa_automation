a58ba99792da8ebbab2f89838abdcf8dc9cbd29a
Garrett Smith <sdga92@gmail.com>
2021-08-06 16:10:34 -0600

    Fixed dropbox TOTP

--- setup_scripts/dropbox.js
+++ setup_scripts/dropbox.js
@@ -20,7 +20,12 @@ chrome.runtime.onMessage.addListener(
             document.querySelector("html > body > div:nth-of-type(12) > div:nth-of-type(1) > div:nth-of-type(2) > div > div > div > div:nth-of-type(1) > div:nth-of-type(2) > div > div > form > div:nth-of-type(2) > button").click();
         } else if (request.dropbox_password) {
             change(document.querySelector(`input[type="password"]`), request.password);
-            document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(3) > button").click();
+            if(document.querySelector("html > body > div:nth-of-type(16) > div > div > div > div > div:nth-of-type(3) > button")){
+                document.querySelector("html > body > div:nth-of-type(16) > div > div > div > div > div:nth-of-type(3) > button").click()
+            }
+            else if (document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(3) > button")) {
+                document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(3) > button").click();
+            }
             setTimeout(() => {
                 let error = document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(2) > div > div > div:nth-of-type(1)");
                 if (error !== null && error.textContent !== null) {
@@ -35,20 +40,46 @@ chrome.runtime.onMessage.addListener(
                 }
             }, 2000);
         } else if (request.dropbox_phone_number) {
-            change(document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(2) > div > div > div:nth-of-type(3) > div:nth-of-type(2) > span > input"), request.number);
-            document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(3) > button:nth-of-type(2)").click();
+            let twofawindow = document.querySelector("#twofactor-enter-phone")
+            if(twofawindow.querySelector("input[type='text']")){
+                change(twofawindow.querySelector("input[type='text']"),request.number )
+            }
+            else {
+                change(document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(2) > div > div > div:nth-of-type(3) > div:nth-of-type(2) > span > input"), request.number);
+            }           
+            if(document.querySelector("button.dig-Button--primary")){
+                document.querySelector("button.dig-Button--primary").click();
+            } else {
+                document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(3) > button:nth-of-type(2)").click();
+            }
             chrome.runtime.sendMessage({
                 dropbox_get_code: true,
             });
         } else if (request.dropbox_code) {
             change(document.querySelector("#phone-code"), request.code);
-            document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(3) > button:nth-of-type(2)").click();
-            setTimeout(() => {
+            if(document.querySelector("button.dig-Button--primary")){
+                document.querySelector("button.dig-Button--primary").click();
+            } else if(document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(3) > button:nth-of-type(2)")) {
                 document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(3) > button:nth-of-type(2)").click();
-                setTimeout(() => {
+            }
+            setTimeout(() => {
+                if(document.querySelector("button.dig-Button--primary")){
+                    document.querySelector("button.dig-Button--primary").click();
+                } else {
                     document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(3) > button:nth-of-type(2)").click();
+                }
+                setTimeout(() => {
+                    if(document.querySelector("button.dig-Button--primary")){
+                        document.querySelector("button.dig-Button--primary").click();
+                    } else {
+                        document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(3) > button:nth-of-type(2)").click();
+                    }
                     setTimeout(() => {
-                        document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(3) > button").click();
+                        if(document.querySelector("button.dig-Button--primary")){
+                            document.querySelector("button.dig-Button--primary").click();
+                        } else {
+                            document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(3) > button").click();
+                        }
                         setTimeout(() => {
                             chrome.runtime.sendMessage({
                                 dropbox_finished: true,
@@ -60,20 +91,40 @@ chrome.runtime.onMessage.addListener(
 
         } else if (request.dropbox_start_sms) {
             document.querySelector("#use-sms").click();
-            document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(3) > button").click();
+            if(document.querySelector("button.dig-Button--primary")){
+                document.querySelector("button.dig-Button--primary").click();
+            } else {
+                document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(3) > button").click();
+            }
             chrome.runtime.sendMessage({
                 dropbox_get_phone_number: true,
             });
         } else if (request.dropbox_start_totp) {
             document.querySelector("#use-app").click();
-            document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(3) > button").click();
+            if(document.querySelector("html > body > div:nth-of-type(16) > div > div > div > div > div:nth-of-type(3) > button")){
+                document.querySelector("html > body > div:nth-of-type(16) > div > div > div > div > div:nth-of-type(3) > button").click()
+            } else if(document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(3) > button")) {
+                document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(3) > button").click();
+            }
             setTimeout(() => {
-                document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(2) > div > ul > li:nth-of-type(2) > button").click();
+                if(document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(2) > div > ul > li:nth-of-type(2) > button")){
+                    document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(2) > div > ul > li:nth-of-type(2) > button").click();
+                } else if(document.querySelector("html > body > div:nth-of-type(16) > div > div > div > div > div:nth-of-type(2) > div > ul > li:nth-of-type(2) > button")) {
+                    document.querySelector("html > body > div:nth-of-type(16) > div > div > div > div > div:nth-of-type(2) > div > ul > li:nth-of-type(2) > button").click();
+                }
+                
                 chrome.runtime.sendMessage({
                     dropbox_get_code: true,
                     totp_secret: document.querySelector("#secret-div").textContent.replace(/\s+/g, '')
                 });
-                document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(3) > button:nth-of-type(2)").click();
+                if(document.querySelector("button.dig-Button--primary")){
+                    document.querySelector("button.dig-Button--primary").click();
+                }
+                else if(document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(3) > button:nth-of-type(2)")){
+                    document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(3) > button:nth-of-type(2)").click();
+                } else if(document.querySelector("html > body > div:nth-of-type(16) > div > div > div > div > div:nth-of-type(3) > button:nth-of-type(2)")){
+                    document.querySelector("html > body > div:nth-of-type(16) > div > div > div > div > div:nth-of-type(3) > button:nth-of-type(2)").click();
+                }
             }, 2000);
         }
     }
@@ -82,12 +133,18 @@ chrome.runtime.onMessage.addListener(
 
 if (window.location.href.includes("dropbox.com/account/security")) {
     setTimeout(() => {
-        document.querySelector("html > body > div:nth-of-type(4) > div > div > div > div:nth-of-type(1) > div > div > div > div > div:nth-of-type(4) > div > div:nth-of-type(2) > label > input").click();
+        if(document.querySelector("html > body > div:nth-of-type(4) > div > div > div > div:nth-of-type(1) > div > div > div > div > div:nth-of-type(3) > div > div:nth-of-type(2) > label > input") != null){
+            document.querySelector("html > body > div:nth-of-type(4) > div > div > div > div:nth-of-type(1) > div > div > div > div > div:nth-of-type(3) > div > div:nth-of-type(2) > label > input").click();
+        } else if(document.querySelector("html > body > div:nth-of-type(4) > div > div > div > div:nth-of-type(1) > div > div > div > div > div:nth-of-type(4) > div > div:nth-of-type(2) > label > input") != null){
+            document.querySelector("html > body > div:nth-of-type(4) > div > div > div > div:nth-of-type(1) > div > div > div > div > div:nth-of-type(4) > div > div:nth-of-type(2) > label > input").click();
+        }
         setTimeout(() => {
             if (document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(3) > button:nth-of-type(2)")) {
                 document.querySelector("html > body > div:nth-of-type(15) > div > div > div > div > div:nth-of-type(3) > button:nth-of-type(2)").click()
             }
-
+            if (document.querySelector("html > body > div:nth-of-type(16) > div > div > div > div > div:nth-of-type(3) > button:nth-of-type(2)")) {
+                document.querySelector("html > body > div:nth-of-type(16) > div > div > div > div > div:nth-of-type(3) > button:nth-of-type(2)").click()
+            }
             if (document.querySelector(`input[type="password"]`) !== null) {
                 chrome.runtime.sendMessage({
                     dropbox_get_password: true,
