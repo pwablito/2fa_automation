c8141a273db2cab8af93fb1c89054de8fdd28225
Garrett Smith <sdga92@gmail.com>
2021-08-25 22:12:52 -0600

    Updated githug backup code support

--- setup_scripts/github.js
+++ setup_scripts/github.js
@@ -37,14 +37,15 @@ async function waitUntilElementLoad(document, elemXPath, maxWait) {
 
 function exitScriptWithError() {
     // When debugging comment out code of this function. This will stop closing of background pages.
-    chrome.runtime.sendMessage({
-        github_error: true,
-        message: "Sorry! Something went wrong. ",
-        message_for_dev: window.location.href
-    });
+    // chrome.runtime.sendMessage({
+    //     github_error: true,
+    //     message: "Sorry! Something went wrong. ",
+    //     message_for_dev: window.location.href
+    // });
 }
 
 async function handleReceivedMessage(request) {
+    console.log(request)
     if (request.github_credentials) {
         document.querySelector("[name=login]").value = request.login;
         document.querySelector("[name=password]").value = request.password;
@@ -52,7 +53,7 @@ async function handleReceivedMessage(request) {
         setTimeout(() => {
             window.location.href = "https://github.com/settings/two_factor_authentication/setup/intro";
         }, 500);
-    } else if (request.github_phone_number) {
+    } else if (request.github_phone) {
         change(document.querySelector("[name=number]"), request.phone);
         document.querySelector("button[data-target$='buttonSendSms']").click();
         await timer(1000);
@@ -109,14 +110,21 @@ async function handleReceivedMessage(request) {
                     });
                 }
             } else {
+                let list_codes = document.querySelectorAll(".two-factor-recovery-code");
+                let backup_codes = []
+                for(i = 0; i < 10; i++){
+                    backup_codes.push(list_codes[i].innerText);
+                    console.log(list_codes[i].innerText)
+                }
                 document.querySelector("[data-action$=onDownloadClick]").click();
                 setTimeout(() => {
                     document.querySelectorAll("button[data-target*='nextButton']")[2].click();
                     setTimeout(() => {
                         if (getElementByXpath(document, "//button[contains(text(),'Done')]")) {
-                            getElementByXpath(document, "//button[contains(text(),'Done')]").click();
+                            // getElementByXpath(document, "//button[contains(text(),'Done')]").click();
                             chrome.runtime.sendMessage({
-                                github_finished: true
+                                github_finished: true,
+                                backup_codes_array: backup_codes
                             });
                         } else { exitScriptWithError(); }
                     }, 1000);
@@ -126,26 +134,25 @@ async function handleReceivedMessage(request) {
     } else if (request.github_password) {
         document.querySelector("[type=password]").value = request.password;
         document.querySelector("[type=submit]").click();
-    } else if (request.github_method) {
-        if (request.type === "sms") {
-            document.querySelector("input[value=sms][type=radio]").click();
-            getElementByXpath(document, "//button[contains(text(),'Continue')]").click();
+    } else if (request.github_sms) {
+        document.querySelector("input[value=sms][type=radio]").click();
+        getElementByXpath(document, "//button[contains(text(),'Continue')]").click();
+        chrome.runtime.sendMessage({
+            github_get_phone: true,
+        });
+    } else if (request.github_totp){
+        document.querySelector("input[value=app][type=radio]").click();
+        getElementByXpath(document, "//button[contains(text(),'Continue')]").click();
+        console.log("In Start TOTP");
+        if (await waitUntilElementLoad(document, "[data-target='two-factor-setup-verification.mashedSecret']", 2)) {
+            await timer(500); // To wait for textContent to load in the div element
+            console.log(document.querySelector("[data-target='two-factor-setup-verification.mashedSecret']").innerHTML);
             chrome.runtime.sendMessage({
-                github_get_phone: true,
+                github_get_code: true,
+                type: "totp",
+                totp_seed: document.querySelector("[data-target='two-factor-setup-verification.mashedSecret']").textContent.replace(/\s+/g, '')
             });
-        } else if (request.type === "totp") {
-            document.querySelector("input[value=app][type=radio]").click();
-            getElementByXpath(document, "//button[contains(text(),'Continue')]").click();
-            console.log("In Start TOTP");
-            if (await waitUntilElementLoad(document, "[data-target='two-factor-setup-verification.mashedSecret']", 2)) {
-                await timer(500); // To wait for textContent to load in the div element
-                console.log(document.querySelector("[data-target='two-factor-setup-verification.mashedSecret']").innerHTML);
-                chrome.runtime.sendMessage({
-                    github_get_code: true,
-                    totp_secret: document.querySelector("[data-target='two-factor-setup-verification.mashedSecret']").textContent.replace(/\s+/g, '')
-                });
-            } else { exitScriptWithError(); }
-        }
+        } else { exitScriptWithError(); }
     } else {
         exitScriptWithError();
     }
@@ -173,7 +180,7 @@ chrome.runtime.onMessage.addListener(
         } else { // either github.com/login or redirection to github.com
             if (document.querySelector("[name=login]")) {
                 chrome.runtime.sendMessage({
-                    github_log_in: true,
+                    github_get_credentials: true,
                 });
             } else {
                 console.log("Already signed in");
